2c302ea6b6a20cb12debab6c00ae681820522bf3
Alex Pishcheiko <oleksandra.pishcheiko@distilled.ie>
2021-01-14 12:47:28 +0000

    ui modal fixes

--- components/templates/OrgReviewModal.tsx
+++ components/templates/OrgReviewModal.tsx
@@ -1,34 +1,26 @@
 import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import { Link, Typography, Avatar, Button, TextField } from '@material-ui/core';
-
-function rand() {
-    return Math.round(Math.random() * 20) - 10;
-  }
-  
-  function getModalStyle() {
-    const top = 50 + rand();
-    const left = 50 + rand();
-  
-    return {
-      top: `${top}%`,
-      left: `${left}%`,
-      transform: `translate(-${top}%, -${left}%)`,
-    };
-  }
+import { Link, Typography, Avatar, Button, TextField, Fade } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    paper: {
-      position: 'absolute',
-      width: 700,
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(2, 4, 3),
-    },
     largePhoto: {
         width: theme.spacing(7),
         height: theme.spacing(7),
+    },
+    modal: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    frameLight: {
+        backgroundColor: theme.palette.background.paper,
+        border: "2px solid #000",
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+        height: "80%",
+        overflowY: "auto",
       },
   }),
 );
@@ -42,7 +34,6 @@ export default function OrgReviewModal(children: any) {
   const { org } = children
 
   const classes = useStyles();
-  const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(false);
 
   const handleModalOpen = () => {
@@ -62,52 +53,57 @@ export default function OrgReviewModal(children: any) {
         <Modal
             open={open}
             onClose={handleModalClose}
+            className={classes.modal}
         >
-            <div style={modalStyle} className={classes.paper}>
-                <Button onClick={handleModalClose} style={{marginLeft:"90%"}}>X</Button>
-                <h2 id="title" style={{marginLeft:"38%"}}>{org}</h2>
-            <Typography style={{fontWeight:"bolder"}}>
-                Mission
-            </Typography>
-            <Typography>
-                {mission}
-            </Typography>
-            <Typography style={{fontWeight:"bolder", marginTop:"15px"}}>
-            Vision
-            </Typography>
-            <Typography>
-                {mission}
-            </Typography>
-            <Typography style={{fontWeight:"bolder",  marginTop:"15px"}}>
-            Country
-            </Typography>
-            <Typography>
-                {country}
-            </Typography>
-            <Typography style={{fontWeight:"bolder",  marginTop:"15px"}}>
-            Social networks
-            </Typography>
-            <Link href={url}>
-                {url}
-            </Link>
-            <Typography style={{fontWeight:"bolder",  marginTop:"15px"}}>
-            Main contact
-            </Typography>
-            <Typography>
-                {contact}
-            </Typography>
-            <Typography style={{fontWeight:"bolder",  marginTop:"15px"}}>
-            Photo ID
-            </Typography>
-            <Avatar src="/photoId.jpg" className={classes.largePhoto}/>
-            <div id="approvalPart" style={{marginTop:"30px"}} >
-                <TextField label="Comments"/>
-                <div>
-                    <Button style={{marginLeft:"60%", color:"green"}} href="#">Accept</Button>
-                    <Button style={{marginLeft:"10%", color:"red"}} href="#">Reject</Button>
+            <Fade in={open}>
+                <div className={classes.frameLight} id="frame">
+                    <div >
+                        <Button onClick={handleModalClose} style={{marginLeft:"90%"}}>X</Button>
+                        <h2 id="title" style={{marginLeft:"38%"}}>{org}</h2>
+                        <Typography style={{fontWeight:"bolder"}}>
+                            Mission
+                        </Typography>
+                        <Typography>
+                            {mission}
+                        </Typography>
+                        <Typography style={{fontWeight:"bolder", marginTop:"15px"}}>
+                        Vision
+                        </Typography>
+                        <Typography>
+                            {mission}
+                        </Typography>
+                        <Typography style={{fontWeight:"bolder",  marginTop:"15px"}}>
+                        Country
+                        </Typography>
+                        <Typography>
+                            {country}
+                        </Typography>
+                        <Typography style={{fontWeight:"bolder",  marginTop:"15px"}}>
+                        Social networks
+                        </Typography>
+                        <Link href={url}>
+                            {url}
+                        </Link>
+                        <Typography style={{fontWeight:"bolder",  marginTop:"15px"}}>
+                        Main contact
+                        </Typography>
+                        <Typography>
+                            {contact}
+                        </Typography>
+                        <Typography style={{fontWeight:"bolder",  marginTop:"15px"}}>
+                        Photo ID
+                        </Typography>
+                        <Avatar src="/photoId.jpg" className={classes.largePhoto}/>
+                        <div id="approvalPart" style={{marginTop:"30px"}} >
+                            <TextField label="Comments"/>
+                            <div>
+                                <Button style={{marginLeft:"60%", color:"green"}} href="#">Accept</Button>
+                                <Button style={{marginLeft:"10%", color:"red"}} href="#">Reject</Button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </Fade>
       </Modal>
     </div>
   );
