6ed1a9b5e75dcdfb5036fdab79bfd991c64bd5df
Alex Pishcheiko <oleksandra.pishcheiko@distilled.ie>
2021-02-01 10:14:01 +0000

    npm build fix

--- pages/[organizationId]/profile.tsx
+++ pages/[organizationId]/profile.tsx
@@ -9,7 +9,7 @@ import {
   } from "@material-ui/core"
   import { makeStyles } from "@material-ui/core/styles"
   import React from "react"
-  import { BasePage, convertTitleToSeoUrl } from "../../components/templates"
+  import { BasePage } from "../../components/templates"
 import {DropzoneArea} from 'material-ui-dropzone'
 import { InferGetServerSidePropsType, GetServerSideProps } from "next"
 import { Organization } from "."
