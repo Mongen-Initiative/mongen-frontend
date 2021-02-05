2c13d133a0941e0ff35e605870ba2239304331f6
Alex Pishcheiko <oleksandra.pishcheiko@distilled.ie>
2021-02-05 15:14:48 +0000

    new homepage top look

--- pages/[organizationId]/index.tsx
+++ pages/[organizationId]/index.tsx
@@ -10,6 +10,7 @@ import {
   CardActions,
   Divider,
   Link,
+  Paper,
 } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import React from "react"
@@ -45,6 +46,13 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     color: theme.palette.secondary.light,
   },
+  aliceCarousel: {
+    position: "relative",
+    width: "100%",
+    margin: "auto",
+    direction: "ltr",
+  },
+
 }));
 
 export interface Organization {
@@ -76,19 +84,44 @@ function Index({ organization }: InferGetServerSidePropsType<typeof getServerSid
           <BasePage className={classes.rootLight} title={organization.name} orgId={organization.id}>
           <title>Mongen Initiative</title>
               <div>
-                <div>
-                <Container maxWidth="sm" className={classes.heroContent}>
-                  <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom >
-                    {organization.name}
-                  </Typography>
-                  <Typography style={{fontSize: "1.8em"}} align="center" color="textSecondary">
-                  {organization.mission}
-                  </Typography>
-                </Container>
-                </div>
+                <Paper elevation={0} style={{marginTop:"60px"}}>
+                   {/* image on the left */}
+                   <div>
+                    <Paper elevation={0} style={{width:"50%", opacity:"0.8", position: "relative", textAlign: "center", color: "white", marginTop:"-50px", marginBottom:"280px"}}>
+                      <img src="childrenBig.jpg" style={{width:"100%"}}></img>
+                      <Typography component="h2" variant="h3" align="center" style={{ marginTop: "-45%", left: "20%", fontWeight:"bolder", textShadow: "black 0.1em 0.1em 0.2em", fontSize:60}}>
+                        {organization.name}
+                      </Typography>
+                      <Paper elevation={0} style={{position: "absolute", height: "100%", overflowY: "auto", overflowX: "hidden", left:"100%",  boxSizing: "border-box", width:"100%", marginTop:"-50px"}}>
+                        {/* text on the right */}
+                        <Typography style={{fontSize: "1.8em",  paddingLeft:"15px", paddingRight:"15px"}} align="center" >
+                          {/* {organization.tagline} */}
+                          Saving children all around Africa
+                        </Typography>
+                      </Paper>
+                    </Paper>
+                    </div>
+                </Paper>
                 <CallToActionButtons orgId={organization.id}/>
                 <Divider />
-                  <Container className={classes.cardGrid} maxWidth="md">
+                <div style={{marginTop:"50px"}}>
+                  <Paper elevation={3} style={{  marginBottom:"5%", height:"350px", backgroundColor:MuiTheme.palette.primary.main, opacity:"0.9"}}>
+                    <div>
+                      <Typography variant="h2" align="center" style={{color:"white", paddingTop:"40px", textShadow: "black 0.1em 0.1em 0.2em"}}>Who we are</Typography>
+                      <Typography align="center" style={{color:"white", marginTop:"30px", marginLeft:"10%", marginRight:"10%", textShadow: "black 0.1em 0.1em 0.2em"}}>
+                        text texttext text text text text text text text text text text text 
+                        text texttext text text text text text text text text text text
+                        text texttext text text text text text text text text text text
+                        text texttext text text text text text text text text text text 
+                      </Typography>
+                      <Button variant="outlined" size="large" href={`/${organization.id}/about`} style={{color: MuiTheme.palette.primary.main, backgroundColor:"#edf2ea", marginTop:"5%", marginLeft:"45%"}}>
+                        Read More
+                      </Button>
+                    </div>
+                  </Paper>
+                  <Divider />
+                </div>                           
+                <Container className={classes.cardGrid} maxWidth="md">
                     <Grid container spacing={4}>
                       {children.map((child) => (
                         <Grid item key={child} xs={12} sm={6} md={4}>
