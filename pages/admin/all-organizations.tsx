d2489489de9fc48a629b3d278903604da6f15c06
Alex Pishcheiko <oleksandra.pishcheiko@distilled.ie>
2021-04-04 18:27:00 +0100

    adding an info message about org review flow

--- pages/admin/all-organizations.tsx
+++ pages/admin/all-organizations.tsx
@@ -77,11 +77,20 @@ import { Button } from "@material-ui/core"
           <title>Admin | Mongen Initiative</title>
             <div style={{marginTop:"30px"}}>
                 <Link style={{marginLeft:"7%"}} href="/admin"> &larr; Back to Admin Panel</Link>
-                <Container maxWidth="sm" className={classes.heroContent}>
+                <Container maxWidth="md" className={classes.heroContent}>
                     <Typography component="h2" variant="h3" align="center" color="textPrimary" gutterBottom >
                         All organizations
                     </Typography>
-                     <div style={{ height: 550, width:"1100px", paddingTop:"50px", marginLeft:"-200px"}}>
+                    <div style={{border:"1px solid", paddingTop:"30px", paddingBottom:"40px", paddingLeft:"30px", marginBottom:"30px", marginTop:"50px", width:"100%"}}>
+                      <Typography align="center" color="primary" style={{marginBottom:"20px"}}> The flow: </Typography>
+                      <Typography color="primary"> 1. User sends a request to create an organization, it is created as  <span style={{fontWeight:"bolder"}}>Draft.</span></Typography>
+                      <Typography color="primary"> 2. Mongen Admin approves an organization by clicking "Summary" &#8211;&#62;	 Approve.</Typography>
+                      <Typography color="primary"> 3. The organization becomes <span style={{fontWeight:"bolder"}}>Pending</span>. Org main contact gets a link to the org admin page.</Typography>
+                      <Typography color="primary"> 4. Organization Admin can edit and publish the organization. It becomes <span style={{fontWeight:"bolder"}}>Published</span>. The website page is now available to anyone in the web.</Typography>
+                      <Typography color="primary"> 5. Mongen Admin can also mark a suspicious org as <span style={{fontWeight:"bolder"}}>Under Review</span>. The website page is still available to anyone in the web.</Typography>
+                      <Typography color="primary"> 6. Mongen Admin can also mark an org as <span style={{fontWeight:"bolder"}}>Disabled</span>. The website page is then becomes unavailable.</Typography>
+                    </div>
+                     <div style={{ height: 550, width:"1100px", paddingTop:"50px", marginLeft:"-10%"}}>
                       <DataGrid rows={organizations} columns={columns} pageSize={7} checkboxSelection />
                     </div>
                 </Container>
